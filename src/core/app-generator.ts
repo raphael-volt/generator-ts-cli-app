d10b6f7e77b92c6fca5ad2f7b0c6a2675345ddbd
raphael-volt <raphael@ketmie.com>
2017-07-13 18:51:21 +0200

    added dependencies compare function

--- src/core/app-generator.ts
+++ src/core/app-generator.ts
@@ -1,20 +1,56 @@
 import { mkdirSync } from "../utils/rmdir-r";
 import { AppDescriptor } from "./app-descriptor";
+import {
+    PACKAGE_JSON
+} from "./filenames";
 import * as fs from 'fs-extra'
 import * as path from 'path'
 import * as mustache from "mustache";
+import { Observable, Observer } from "rxjs";
+
+const tplDir: string = path.join(
+    path.dirname(path.dirname(__dirname)),
+    "templates")
+const TPL_EXT: string = ".tpl"
 
 export class AppGenerator {
 
-    update(dir: string): void{
-
+    private cwd: string
+    update(dir: string): Observable<any> {
+        this.cwd = dir
+        return Observable.create(this._update)
     }
-    
+
+    private local = (...args): string => {
+        return path.join.apply(null, [this.cwd].concat(args))
+    }
+
+    private template = (...args): string => {
+        return path.join.apply(null, [tplDir].concat(args))
+    }
+
+    private _update = (observer: Observer<any>) => {
+        const local = this.local
+        const errorFn = error => observer.error(error)
+        const nextFn = pkg => {
+            observer.next(pkg)
+            observer.complete()
+        }
+
+        fs.readJSON(local(PACKAGE_JSON))
+            .then(pkg => {
+                fs.readJSON(this.template(PACKAGE_JSON + TPL_EXT))
+                .then((tpl: PackageJSON) => {
+                    const depsChange: boolean = dependenciesDif(pkg, tpl)
+                    
+                })
+                nextFn(pkg)
+            }).catch(errorFn)
+    }
+
     build(descriptor: AppDescriptor, dir: string): void {
         this.createDirs(dir)
-        const tplDir: string = path.join(
-            path.dirname(path.dirname(__dirname)),
-            "templates")
+
         let src: string
         let dst: string
         let f: string
@@ -57,14 +93,14 @@ export class AppGenerator {
         f = "package.json"
         src = path.join(tplDir, f + tplExt)
         dst = path.join(dir, f)
-        this.template(src, dst, descriptor)
+        this.saveTemplate(src, dst, descriptor)
     }
 
     private copy(src: string, dst: string) {
         fs.writeFileSync(dst, fs.readFileSync(src))
     }
 
-    private template(src: string, dst: string, templateData: any) {
+    private saveTemplate(src: string, dst: string, templateData: any) {
         fs.writeFileSync(dst, mustache.render(fs.readFileSync(src).toString(), templateData))
     }
 
@@ -76,4 +112,94 @@ export class AppGenerator {
         mkdirSync(path.join(dir, "bin"))
 
     }
+}
+
+export interface PackageJSON {
+    [name: string]: any
+    name?: string
+    version?: string
+    description?: string
+    repository?: {
+        type?: string
+        url?: string
+    }
+    scripts?: {
+        [name: string]: string
+    }
+    author?: {
+        name?: string
+        email?: string
+    }
+    main?: string
+    typings?: string
+    files?: string[]
+    devDependencies?: {
+        [name: string]: string
+    }
+    dependencies?: {
+        [name: string]: string
+    }
+}
+
+const dependenciesDif = (target: PackageJSON, src: PackageJSON): boolean => {
+
+    const removed: [boolean, string, string | undefined][] = []
+    const added: [boolean, string, string][] = []
+
+    if (target.dependencies == undefined)
+        target.dependencies = {}
+    if (target.devDependencies == undefined)
+        target.devDependencies = {}
+
+    const asDep = (pkg: PackageJSON, dep: string): boolean => {
+        return pkg.dependencies[dep] !== undefined
+    }
+
+    const asDevDep = (pkg: PackageJSON, dep: string): boolean => {
+        return pkg.devDependencies[dep] !== undefined
+    }
+
+    const depEquals = (a: PackageJSON, b: PackageJSON, name: string): boolean | undefined => {
+        if (asDep(a, name) && asDep(b, name))
+            return a.dependencies[name] == b.dependencies[name]
+        return undefined
+    }
+
+    const devDepEquals = (a: PackageJSON, b: PackageJSON, name: string): boolean | undefined => {
+        if (asDevDep(a, name) && asDevDep(b, name))
+            return a.devDependencies[name] == b.devDependencies[name]
+        return undefined
+    }
+
+    let d: any
+
+    for (d in src.dependencies) {
+        if (asDevDep(target, d)) {
+            removed.push([false, d, undefined])
+            delete (target.devDependencies[d])
+        }
+    }
+
+    for (d in src.devDependencies) {
+        if (asDep(target, d)) {
+            removed.push([true, d, undefined])
+            delete (target.dependencies[d])
+        }
+    }
+
+    for (d in src.dependencies) {
+        if (depEquals(src, target, d) === true)
+            continue
+        target.dependencies[d] = src.dependencies[d]
+        added.push([true, d, src.dependencies[d]])
+    }
+
+    for (d in src.devDependencies) {
+        if (devDepEquals(src, target, d) === true)
+            continue
+        target.devDependencies[d] = src.devDependencies[d]
+        added.push([true, d, src.devDependencies[d]])
+    }
+
+    return added.length > 0 || removed.length > 0
 }
--- test/update.spec.ts
+++ test/update.spec.ts
@@ -2,8 +2,11 @@ import * as chai from 'chai';
 import * as sinon from 'sinon';
 import * as mocha from 'mocha';
 import * as path from 'path'
+import * as fs from 'fs-extra'
 import { existsSync } from 'fs'
 import { rmdir, rmdirSync } from "../src/utils/rmdir-r";
+import { AppGenerator } from "../src/core/app-generator";
+
 const expect = (target: any, message?: string): Chai.Assertion => {
     return chai.expect(target, message)
 }
@@ -18,15 +21,37 @@ const expectNot = (target: any, message?: string): Chai.Assertion => {
 
 const appDir: string = path.join(path.dirname(__dirname), "tests/test-app")
 
+let gen: AppGenerator = new AppGenerator()
+const config = {
+    name: "test-app",
+    author: {
+        name: "test-app author",
+        email: "testapp@gmail.com",
+        repository: "https://github.com/test-app/test-app.git"
+    },
+    command: "testApp"
+}
+
 describe('Update', () => {
 
-    it("should delete test-app", (done) => {
-
-        rmdir(appDir, done)
+    it("should create app if not", function (done) {
+        this.timeout(2000)
+        if (fs.existsSync(appDir))
+            return done()
+        gen.build(config, appDir)
+        expectBe(fs.existsSync(appDir)).true
+        done()
     })
 
-    it("should test-app be deleted", () => {
-        expectBe(existsSync(appDir)).false
+    it("should update app without changes", (done) => {
+
+        gen.update(appDir).subscribe(
+            (pkg: any) => {
+                expectBe(pkg.name).equals(config.name)
+            },
+            done,
+            done
+        )
     })
 })
 
